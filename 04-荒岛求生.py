b8d681fd74a08bef5bd083702738aae572baad6c
wz1990911 <2905682123@qq.com>
2018-06-28 10:03:38 +0800

    答辩

--- 04-荒岛求生.py
+++ 04-荒岛求生.py
@@ -46,16 +46,19 @@ def deng():
     print("斯...头好痛")
     xuan()
 def print_d():
+        print("=".center(50,"="))
         print("1------开始探险")
         print("2------查看属性")
         print("3------查看物品")
         print("4------换购物品")
         print("5------补充能量")
-        print("6------丢弃物品")
+        print("6------转换东西")
         print("0------退出系统")
+        print("=".center(50,"="))
 def xuan():
     print("欢迎主人来到荒岛体验系统".center(50,"*"))
     tian()
+    tian_1()
     while True:
         print_d()
         a = input("请输入您的选择")
@@ -83,19 +86,31 @@ def tian():
     zi["shui"] = 100
     zi["ti"] = 100
     list3.append(zi)
+def tian_1():
+    zi = {}
+    zi["yuan"] = 0
+    zi["rou"] = 0
+    zi["yao"] = 0
+    zi["kuang"] =0 
+    zi["cai"] = 0
+    zi["te"] = 0
+    zi["zhuan"] =0
+    zi["jin"] = 0
+    list2.append(zi)
 def print_1():
-    print("1---处级探险(可以获得水源,野果.木材)")
-    print("2---高级探险(可以碰到猎物获得食物,草药,矿物)")
-    print("0---取消探险")
-    b = int(input("请输入选择"))
-    zi={}
     yuan = 0
     rou = 0
     yao = 0
-    kuang = 0
-    shui = 0
-    ye = 0
+    kuang =0 
     cai = 0
+    te = 0
+    zhuan =0 
+    print("1---处级探险(可以获得水源,木材)")
+    print("2---中级探险(可以碰到猎物获得食物,草药,矿物)")
+    print("3---高级探险(可以获得特殊材料和转化石几率较小)")
+    print("0---取消探险")
+    b = int(input("请输入选择"))
+    zi={}
     if b == 1:
         while True:
             a = random.randint(1,5)#1---水源 2----野果 3---木材 其他---什么也没有
@@ -110,20 +125,10 @@ def print_1():
                 ji = int(input("请选择"))
                 if ji == 2:
                     break
-            elif a == 2:
-                print_jian()
-                ye = random.randint(1,10)
-                print("恭喜你获得了%d个野果可补充少量体力"%ye)
-                ye += ye
-                print("1------继续探险")
-                print("2------退出探险")
-                ji = int(input("请选择"))
-                if ji == 2:
-                    break
             elif a == 3:
                 print_jian()
                 cai = random.randint(1,10)
-                print("恭喜你获得了%d根木材可创造东西"%cai)
+                print("恭喜你获得了%d根木材可兑换"%cai)
                 cai+=cai
                 print("1------继续探险")
                 print("2------退出探险")
@@ -138,9 +143,6 @@ def print_1():
                 if ji == 2:
                     break
     
-    zi["yuan"] = yuan
-    zi["ye"] = ye
-    zi["cai"] = cai
                 
     if b == 2:
         while True:
@@ -179,7 +181,7 @@ def print_1():
             elif a == 3:
                 print_jian()
                 kuang = random.randint(1,10)
-                print("恭喜获得%d块矿物可创造东西"%kuang)
+                print("恭喜获得%d块矿物可兑换东西"%kuang)
                 kuang+=kuang
                 print("1------继续探险")
                 print("2------退出探险")
@@ -193,10 +195,41 @@ def print_1():
                 ji = int(input("请选择"))
                 if ji == 2:
                     break
-    zi["rou"] = rou
-    zi["yao"] = yao
-    zi["kuang"] = kuang
-    list2.append(zi)
+    if b == 3:
+        while True:
+            print_jian()
+            a = random.randint(1,10)
+            if a == 7:
+                print("恭喜你获得1块特殊材料可兑换上帝的金手指")
+                te+=1 
+                print("1------继续探险")
+                print("2------退出探险")
+                ji = int(input("请选择"))
+                if ji == 2:
+                    break
+            elif a == 2:
+                print("恭喜你获得1快转换石可转换成任何东西但需要有上帝的金手指")
+                zhuan+=1
+                print("1------继续探险")
+                print("2------退出探险")
+                ji = int(input("请选择"))
+                if ji == 2:
+                    break
+            else:
+                print("什么也没有")
+                print("1------继续探险")
+                print("2------退出探险")
+                ji = int(input("请选择"))
+                if ji == 2:
+                    break
+    for position,i in enumerate(list2):    
+        i["yuan"] = yuan+i["yuan"]
+        i["cai"] = cai+i["cai"]
+        i["rou"] = rou+i["rou"]
+        i["yao"] = yao+i["yao"]
+        i["kuang"] = kuang+i["kuang"]
+        i["te"] = te+i["te"]
+        i["zhuan"] = zhuan+i["zhuan"]
 
 def print_2():
     print("健康\t体力\t水分")
@@ -204,32 +237,33 @@ def print_2():
         print(i["jian"],"\t",i["ti"],"\t",i["shui"])   
 def print_jian():
     for position,i in enumerate(list3):
-        i["jian"] = i["jian"]-3
-        i["ti"] = i["ti"]-3
-        i["shui"] = i["shui"]-3
-        if i["jian"] < 20:
+        i["jian"] = i["jian"]-1
+        i["ti"] = i["ti"]-1
+        i["shui"] = i["shui"]-1
+        if i["jian"] < 3:
             print("j健康快到极限了")
             print("请尽快医治")
-        elif i["ti"] < 10:
+        elif i["ti"] < 3:
             print("体力快到极限了")
             print("请尽快补充体力")
-        elif i["shui"] < 15:
+        elif i["shui"] < 3:
             print("水分快到极限了")
             print("请尽快补充水分")
 def print_3():
-    print("水\t野果\t木材\t肉\t草药\t矿物")
+    print("水\t木材\t肉\t草药\t矿物\t特殊材料\t转换石 \t金手指")
     for position,i in enumerate(list2):
-        print(i["yuan"],"\t",i["ye"],"\t",i["cai"],"\t",i["rou"],"\t",i["yao"],"\t",i["kuang"])
+        print(i["yuan"],"\t",i["cai"],"\t",i["rou"],"\t",i["yao"],"\t",i["kuang"],"\t",i["te"],"\t","\t",i["zhuan"],"\t"," ",i["jin"])
 
 def print_4():
     while True:
         print("欢迎来到换购商店".center(50,"*"))
         print("本商店任何东西都需等价交换")
-        print("换购的东西为木材或矿物")
+        print("用木材或矿物换购")
         print("材料不同换购的数量也不同")
         print("1------水")
         print("2------肉")
         print("3------药")
+        print("4------上帝的金手指 注:指此物品只能用特殊物品换购")
         print("0------退出")
         a = int(input("请输入序号"))
         if a == 1:
@@ -238,9 +272,23 @@ def print_4():
             xuan_1()
         elif a == 3:
             xuan_2()
+        elif a == 4:
+            xuan_3()
         elif a == 0:
             break
-        
+def xuan_3():
+    jin = 0
+    for position,i in enumerate(list1):
+        if i["te"] > 1:
+            i["te"] = i["te"]-1
+            jin+=1
+            i["jin"] = i["jian"]+jin
+            print("恭喜获得上帝的金手指1个")
+            print("1----继续兑换")
+            print("2----")
+        else:
+            print("材料不够")
+            break
 def print_xuan():
     print("1------木材")
     print("1------矿物")
@@ -344,5 +392,20 @@ def print_5():
                         print("增加10点健康值")
                 else:
                     print("材料不足")
-                
+def print_6():
+    zi{}
+    print("欢迎来到转换商店".center("*"))
+    while True:
+        print("1----铁镐花费一个金手指")
+        print("2----电钻花费5个金手指")
+        a = print("请输入选择要转换的东西")
+        for position,i in enumerate(list2):
+            if a == 1:
+                if i["jin"] > 1
+                    i["jin"] = i["jin"]-1
+                    zi["tie"] =" 铁镐"
+                    print("恭喜你获得铁镐")
+                else:
+                    print("材料不够")
+    
 cuang()
